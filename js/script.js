38d1a8d8cc57d521feacb6c0a8d6cd1de13c1b4d
Rizus912 <koshechkin.1988@mail.ru>
2019-01-29 03:10:51 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,14 +3,35 @@ var menu = document.getElementById("menu");
 var lines = document.getElementById("lines");
 var content = document.getElementById("contentShadow");
 var cont = document.getElementById("content");
+var baloon = document.getElementById("baloon");
+var airship = document.getElementById("airship");
+var plane = document.getElementById("plane");
+
+
 var nav = false;
 var color;
+var page = document.getElementsByClassName('page');
+for (i=0;i<page.length;i++) { page[i].style.display="none";}
+document.getElementById('b').style.display="block";
+window.onload=function(){
+  document.getElementById('preloader').style.display="none";
+  var page = document.getElementsByClassName('page');
+  for (i=0;i<page.length;i++) { page[i].style.display="none";}
+  document.getElementById('b').style.display="block";
 
-window.onload = function(){
-  var link = document.querySelector('link[rel=import]');
-      var contentPaste = link.import.querySelector('#paste');
-      document.cont.appendChild(contentPaste.cloneNode(true));
+  baloon.style.bottom="15%";
+  airship.style.top="4%";
+  airship.style.left= "-280px";
+  plane.style.right= "2%";
+
+
 }
+
+
+
+
+
+
 
 
 
@@ -23,6 +44,7 @@ goNav = function(){
     lines.style.borderBottom="17px double #F8F8F8";
     lines.style.borderTop="6px solid #F8F8F8";
     lines.style.opacity= "0.9";
+
   }else{
     menu.style.left="-250px";
     content.style.zIndex= "-250";
